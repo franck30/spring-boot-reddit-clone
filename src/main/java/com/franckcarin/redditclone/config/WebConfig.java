ff0312f5eff3489d767f02e994d57c17a2b5749d
tchuitiocarin <tchuitiocarin@gmail.com>
2021-05-29 23:24:39 +0100

    add permit all on post api

--- src/main/java/com/franckcarin/redditclone/config/WebConfig.java
+++ src/main/java/com/franckcarin/redditclone/config/WebConfig.java
@@ -4,6 +4,7 @@ package com.franckcarin.redditclone.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -19,5 +20,14 @@ public class WebConfig implements WebMvcConfigurer{
             .allowedHeaders("*")
             .exposedHeaders("Authorization")
             .allowCredentials(true);
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("swagger-ui.html")
+            .addResourceLocations("classpath:/META-INF/resources/");
+
+    registry.addResourceHandler("/webjars/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/");
   }
 }
